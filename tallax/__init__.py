4677123025448f0b5281774bc93bc1bd6aff590c
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-11-21 08:31:20 +0800

    Update __init__.py

--- tallax/__init__.py
+++ tallax/__init__.py
@@ -1,5 +1,5 @@
 
-from .sort import lax_sort_pallas
-from .top_k import lax_topk_pallas
+from .sort import lax_sort_pallas as sort
+from .top_k import lax_topk_pallas as top_k
 
-__all__ = ["lax_sort_pallas", "lax_topk_pallas"]
+__all__ = ["sort", "top_k"]
