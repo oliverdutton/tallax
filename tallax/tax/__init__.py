a83879447abc7837e73b1a9695d9888f7c01b294
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-11-23 01:03:36 +0800

    Update __init__.py

--- tallax/tax/__init__.py
+++ tallax/tax/__init__.py
@@ -1,6 +1,6 @@
 from tallax.tax.cumsum import cumsum
 from tallax.tax.gather import gather
 from tallax.tax.sort import sort
-from tallax.tax.top_k import top_k
+from tallax.tax.top_k import top_k, top_dynamic_k
 
-__all__ = ["cumsum", "gather", "sort", "top_k"]
+__all__ = ["cumsum", "gather", "sort", "top_k", "top_dynamic_k"]
