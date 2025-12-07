c29fc7f59a1f48cb961ffe09792affb98a659baf
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-12-07 21:43:35 +0800

    Update __init__.py

--- tallax/tax/__init__.py
+++ tallax/tax/__init__.py
@@ -1,7 +1,6 @@
-from tallax.tax.cumsum import cumsum
 from tallax.tax.gather import gather
 from tallax.tax.sort import sort, sort_xla_equivalent
 from tallax.tax.top_k import top_k, top_dynamic_k
 from tallax.tax.fused_sampling import top_p_and_sample
 
-__all__ = ["cumsum", "gather", "sort", "top_k", "top_dynamic_k", "sort_xla_equivalent", "top_p_and_sample"]
+__all__ = ["gather", "sort", "top_k", "top_dynamic_k", "sort_xla_equivalent", "top_p_and_sample"]
