f535fc5a37f1a99c84ece0c69bf4f6d1a1bc748e
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-12-17 20:44:35 +0900

    Update take_along_axis_test.py

--- tests/take_along_axis_test.py
+++ tests/take_along_axis_test.py
@@ -6,7 +6,7 @@ import numpy as np
 from tallax._src.gather import take_along_axis
 from tallax._src.utils import is_cpu_platform
 
-@pytest.mark.parametrize("num_tokens", [8, 16, 13])
+@pytest.mark.parametrize("num_tokens", [8, 16, 13, 128, 137, 256, 279])
 @pytest.mark.parametrize("vocab_size", [128, 256, 300])
 @pytest.mark.parametrize("k", [64, 128, 200, 300])
 @pytest.mark.parametrize("axis", [0, 1])
