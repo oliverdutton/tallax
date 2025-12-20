4a7e394df84d875fce73ece19db5ebac632b61b9
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-12-20 18:29:51 +0900

    Update approx_max_k_test.py

--- tests/approx_max_k_test.py
+++ tests/approx_max_k_test.py
@@ -10,12 +10,13 @@ from tallax._src.test_utils import verify_topk_output
 @pytest.mark.parametrize("k", [1, 2, 3, 17, 32, 64, 128])
 @pytest.mark.parametrize("recall_target", [0.5, 0.8, 0.95, 0.99])
 @pytest.mark.parametrize("shape", [(128, 8192)])
+@pytest.mark.parametrize("use_lax_approx_max_k_algorithm", [False, True])
 @pytest.mark.skipif(is_cpu_platform(), reason="approx_max_k tests require TPU/GPU")
-def test_approx_max_k(dtype, k, recall_target, shape):
+def test_approx_max_k(dtype, k, recall_target, shape, use_lax_approx_max_k_algorithm):
     """Test approx_max_k with iota reshaped."""
     operand = jnp.arange(shape[0] * shape[1], dtype=dtype).reshape(shape).astype(dtype)
 
-    outputs = tax.approx_max_k(operand, k=k, recall_target=recall_target)
+    outputs = tax.approx_max_k(operand, k=k, recall_target=recall_target, use_lax_approx_max_k_algorithm=use_lax_approx_max_k_algorithm)
     recall = verify_topk_output(operand, outputs, axis=1, approximate=True).mean()
     # double the aim, to allow for noise. Maxmimum recall check for noise as well.
     test_recall_threshold = min(0.95, 1 - 2*(1-recall_target))
