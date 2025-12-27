37b1b0ef4b5c86890e82e5df4d4bef4e504bf66f
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-12-28 00:01:16 +0700

    Update divide_and_filter_topk.py

--- tallax/_src/divide_and_filter_topk.py
+++ tallax/_src/divide_and_filter_topk.py
@@ -437,11 +437,9 @@ def top_dynamic_k(
           - topk_vals: Top-k values of shape [num_tokens, max_k].
           - topk_idxs: Top-k indices of shape [num_tokens, max_k].
   """
-  unpadded_num_tokens = logits.shape[0]
 
   # pad in first dimension for block spec
   # padding will count as immediately converged as it pads with minimum finite value (not -inf where comparison is difficult to define when checking for convergence)
-  logits = pad(logits, (1, NUM_LANES), val='min')
   num_tokens, vocab_size = logits.shape
   num_tokens_padded = ceil_multiple(num_tokens, block_token)
     
@@ -515,12 +513,12 @@ def top_dynamic_k(
   )(logits, k, k)
   topk_vals, topk_idxs, valid, depths, cutoff_vals = outputs
 
-  topk_vals, topk_idxs = (x[:unpadded_num_tokens,:max_k] for x in (topk_vals, topk_idxs))
+  topk_vals, topk_idxs = (x[:num_tokens,:max_k] for x in (topk_vals, topk_idxs))
   valid = valid.squeeze().astype(bool)
 
   if guarantee_convergence:
     return topk_vals, topk_idxs
-  return topk_vals, topk_idxs, valid, depths[:unpadded_num_tokens], cutoff_vals[:unpadded_num_tokens]
+  return topk_vals, topk_idxs, valid, depths[:num_tokens], cutoff_vals[:num_tokens]
 
 @functools.partial(
     jit,
