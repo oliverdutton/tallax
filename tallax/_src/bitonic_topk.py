bad6a951d1512541150a6348c0246a137bbf1878
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-12-27 20:47:32 +0700

    Update bitonic_topk.py

--- tallax/_src/bitonic_topk.py
+++ tallax/_src/bitonic_topk.py
@@ -64,8 +64,9 @@ def _compute_padded_shape(unpadded_dim0: int, unpadded_dim1: int, k: int) -> tup
   dim0s = [2**i for i in range(log2(NUM_SUBLANES), log2(NUM_LANES)+1)
     if 2**i >= unpadded_dim0]
   shapes = [
-    (dim0, ceil_multiple(unpadded_dim1,
-      NUM_LANES * NUM_LANES // dim0))
+    (dim0, ceil_multiple(
+    ceil_multiple(unpadded_dim1,
+      NUM_LANES * NUM_LANES // dim0), max(k, NUM_SUBLANES))
     for dim0 in dim0s]
   # take minimal num elements, larger dim0 on ties as cross tile ops are faster than cross lane
   return sorted(shapes, key=lambda x: (x[0] * x[1], -x[0]))[0]
