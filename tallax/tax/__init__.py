887a2560009df3481cf29720f8d6a694e1ee8363
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-12-29 16:11:26 +0700

    Update __init__.py

--- tallax/tax/__init__.py
+++ tallax/tax/__init__.py
@@ -12,14 +12,15 @@ Exports:
   approx_max_k: TPU-optimized approximate top-k (jax.lax.approx_max_k counterpart).
   cumsum: TPU-optimized cumulative sum (jax.lax.cumsum counterpart).
 """
-from tallax.tax.bitonic import bitonic_sort_in_vmem as sort
-from tallax.tax.topk import top_k
+from tallax.tax.bitonic import bitonic_sort_in_vmem as sort, bitonic_topk_in_vmem as bitonic_top_k
+from tallax.tax.divide_and_filter_topk.topk import topk as top_k
 from tallax.tax.cumsum import cumsum
 from tallax.tax.approx_max_k import approx_max_k
 
 __all__ = [
     "sort",
     "top_k",
+    "bitonic_top_k",
     "approx_max_k",
     "cumsum",
 ]
