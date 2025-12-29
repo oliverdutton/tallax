d22e6cdb32fd567d75b35fae6173ad184a6fbb44
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-12-29 13:16:04 +0700

    Update gather.py

--- tallax/tax/gather.py
+++ tallax/tax/gather.py
@@ -6,43 +6,36 @@ from jax import jit, lax
 from jax.experimental import pallas as pl
 from jax.experimental.pallas import tpu as pltpu
 
-from tallax.tax.utils import NUM_LANES, NUM_SUBLANES, pad
+from tallax.tax.utils import NUM_LANES, NUM_SUBLANES, pad, split_args_to_chunks
 
+@functools.partial(split_args_to_chunks, num_args=2)
 def take_along_axis_arrays(val, idx, axis):
   shape = idx.shape
   tile_shape = (NUM_SUBLANES, NUM_LANES)
   val, idx = (pad(x, tile_shape, val=0) for x in (val, idx))
-  def _gather_arrays(val, idx):
-    # Initialize accumulators
-    accumulators = [
-        jnp.zeros(tile_shape, dtype=val.dtype)
-        for _ in range(idx.shape[axis] // tile_shape[axis])
-    ]
-    for val_offset in range(0, val.shape[axis], tile_shape[axis]):
-      # Load values for this block once
-      val_tile = lax.slice_in_dim(val, val_offset, val_offset+tile_shape[axis], axis=axis)
-  
-      # Apply to all K blocks
-      for idx_offset in range(0, idx.shape[axis], tile_shape[axis]):
-        idx_tile = lax.slice_in_dim(idx, idx_offset, idx_offset+tile_shape[axis], axis=axis)
-        mask = (idx_tile >= val_offset) & (idx_tile < val_offset + tile_shape[axis])
-        gather_tile = jnp.take_along_axis(
-            val_tile,
-            (idx_tile - val_offset) % tile_shape[axis],
-            axis=axis
-        )
-        i = idx_offset // tile_shape[axis]      
-        accumulators[i] = jnp.where(mask, gather_tile, accumulators[i])
-    return jnp.concatenate(accumulators, axis=axis)
+  # Initialize accumulators
+  accumulators = [
+      jnp.zeros(tile_shape, dtype=val.dtype)
+      for _ in range(idx.shape[axis] // tile_shape[axis])
+  ]
   batch_axis = 1 - axis
   assert val.shape[batch_axis]==idx.shape[batch_axis]
-  return jnp.concatenate(
-    [_gather_arrays(v, i)
-      for v, i in zip(*map(lambda arr: jnp.split(
-        arr, arr.shape[batch_axis] // tile_shape[batch_axis], axis=batch_axis), (val, idx)))
-    ],
-    axis=batch_axis
-  )[:shape[0], :shape[1]]
+  for val_offset in range(0, val.shape[axis], tile_shape[axis]):
+    # Load values for this block once
+    val_tile = lax.slice_in_dim(val, val_offset, val_offset+tile_shape[axis], axis=axis)
+
+    # Apply to all K blocks
+    for idx_offset in range(0, idx.shape[axis], tile_shape[axis]):
+      idx_tile = lax.slice_in_dim(idx, idx_offset, idx_offset+tile_shape[axis], axis=axis)
+      mask = (idx_tile >= val_offset) & (idx_tile < val_offset + tile_shape[axis])
+      gather_tile = jnp.take_along_axis(
+          val_tile,
+          (idx_tile - val_offset) % tile_shape[axis],
+          axis=axis
+      )
+      i = idx_offset // tile_shape[axis]      
+      accumulators[i] = jnp.where(mask, gather_tile, accumulators[i])
+  return jnp.concatenate(accumulators, axis=axis)[:shape[0], :shape[1]]
 
   
 def take_along_axis_refs(values_ref, indices_ref, output_ref, *, axis: int):
