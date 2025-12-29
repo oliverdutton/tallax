8e311108166478371d06e291453d6658767f1a4a
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-12-29 07:31:13 +0700

    Fix descending sort by passing kwargs through decorator (#143)

--- tallax/_src/bitonic_sort_core.py
+++ tallax/_src/bitonic_sort_core.py
@@ -383,8 +383,8 @@ def _bitonic_sort_substages_array_or_refs(inputs,
   if is_ref:
     pl.loop(0, grid_size)(process_block)
   else:
-    outputs = [process_block(outer_i) for outer_i in range(grid_size)]
-    return outputs
+    return transpose_list_of_lists([
+      process_block(outer_i) for outer_i in range(grid_size)])
 
 
 # Partial functions for cleaner usage
@@ -491,7 +491,7 @@ def bitonic_sort_maybe_rolled(operands: list[jax.Array], num_keys: int = 1, desc
     else:
       # use the transpose refs
       assert transpose_refs is not None, "transpose_refs required when not fully unrolling"
-      
+
       for i, arr in enumerate(_rejoin(arrs_tiles)):
         # cut transpose refs if too large
         transpose_refs[i] = transpose_refs[i].at[:arr.shape[0]]
