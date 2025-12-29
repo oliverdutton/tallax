2a101cd79272253e99609c36ee83a0e3cb72fed2
oliver <44170519+oliverdutton@users.noreply.github.com>
2025-12-29 15:05:34 +0700

    Update sampling.py

--- tallax/vllm/sampling.py
+++ tallax/vllm/sampling.py
@@ -12,35 +12,25 @@ from jax.experimental.pallas import tpu as pltpu
 from jax.experimental.custom_partitioning import custom_partitioning
 from jax.sharding import Mesh, NamedSharding, PartitionSpec as P
 
-from tallax.tax.bitonic.topk import bitonic_topk_arrays, max_arrays
+from tallax.tax.bitonic.topk import bitonic_topk_arrays as bitonic_topk
 from tallax.vllm.top_p_and_sample import top_p_and_sample
 from tallax.tax.divide_and_filter_topk.topk import top_dynamic_k
 from tallax.tax.utils import NUM_LANES, NUM_SUBLANES, pad, log2, iota_tile, transpose_list_of_lists
 
-def _topk_with_sharding(logits: jax.Array, k: jax.Array, replace_val):
-    def _topk_arrays(logits: jax.Array, k: jax.Array):
-      if logits.shape[-1] <= 0:
-        # for small sizes just do direct top-k. Constant runtime
-        idxs = jax.lax.broadcasted_iota(jnp.int32, logits.shape, 1)
-        topk_logits, topk_idxs = bitonic_topk_arrays([logits, idxs], k=NUM_LANES)
-        topk_logits = jnp.where(
-          jnp.arange(NUM_LANES)[None, :] < k[:,None],
-          topk_logits,
-          replace_val
-        )
-        return topk_logits, topk_idxs
+def _topk_with_sharding(logits: jax.Array, k: jax.Array, replace_val: float | int, max_k: int, num_bins, bins_topm_schedule: tuple[int,...]):
+    def _topk(logits: jax.Array, k: jax.Array):
       return top_dynamic_k(
         logits,
         k=k,
-        max_k=NUM_LANES,
+        max_k=max_k,
         guarantee_convergence=True,
-        num_bins=256,
-        bins_topm_schedule=(5,9),
+        num_bins=num_bins,
+        bins_topm_schedule=bins_topm_schedule,
         replace_val=replace_val)
     
     @custom_partitioning
     def sharded_topk(logits, k):
-      return _topk_arrays(logits, k)
+      return _topk(logits, k)
     
     def infer_sharding_from_operands(mesh, arg_shapes, result_shape):
       logits_spec = arg_shapes[0].sharding.spec
@@ -52,7 +42,7 @@ def _topk_with_sharding(logits: jax.Array, k: jax.Array, replace_val):
       axis_name = arg_shardings[0].spec[1]
     
       def shmap_fn(logits, k):
-        topk_logits, topk_idxs = _topk_arrays(logits, NUM_LANES)
+        topk_logits, topk_idxs = _topk(logits, k)
         if axis_name is None:
           return topk_logits, topk_idxs
         # convert idxs to global frame
@@ -60,7 +50,7 @@ def _topk_with_sharding(logits: jax.Array, k: jax.Array, replace_val):
         topk_idxs += i * logits.shape[1]
         # all-gather and top-k
         operands = [jax.lax.all_gather(x, axis_name, axis=1) for x in (topk_logits, topk_idxs)]
-        topk_logits, topk_idxs = bitonic_topk(operands, k=NUM_LANES)
+        topk_logits, topk_idxs = bitonic_topk(operands, k=max_k)
         topk_logits = jnp.where(
           jax.lax.broadcasted_iota(jnp.int32, topk_logits.shape, 1) < k[:,None],
           topk_logits,
@@ -77,12 +67,16 @@ def _topk_with_sharding(logits: jax.Array, k: jax.Array, replace_val):
     return sharded_topk(logits, k)
 
 
-def topk_topp_and_sample(rng_key, logits, tpu_sampling_metadata):
+def topk_topp_and_sample(rng_key, logits, tpu_sampling_metadata, max_k=NUM_LANES, num_bins=256, bins_topm_schedule=(5,9)):
   vocab_size = logits.shape[1]
   topk_logits, topk_idxs = _topk_with_sharding(
     logits,
     k=tpu_sampling_metadata.top_k,
-    replace_val=-1e12)
+    replace_val=-1e12,
+    max_k=max_k,
+    num_bins=num_bins,
+    bins_topm_schedule=bins_topm_schedule,
+  )
   return top_p_and_sample(
     topk_logits, topk_idxs,
     rng_key,
